4992444ce1fff15bb7b903ae22b5526a426bb4d3
Matthew <matthew.kolesnik@gmail.com>
2026-03-10 12:49:09 -0400

    lab9-1

---
+++ labs/lab9-1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int array[] = {1, 3, 5, 7, 9};
+
+    for (int i = 0; i < 5; i++)
+        printf("%d ", array[i]);
+    printf("\n");
+    
+    return 0;
+}
---
+++ labs/lab9-1/task2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int data[] = {4, 8, 15, 16, 23, 42};
+
+    int size = sizeof(data);
+    printf("Size of the array: %d\n", size);
+
+    return 0;
+}
